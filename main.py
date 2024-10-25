061be2e5726883908d181dbefcfebf5a5c82df5d
Tfregues <rafaelitoto895@gmail.com>
2024-10-25 14:34:56 -0300

    syntax and argument fixed

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import base
 def run():
     c = 0
     while True:
-
-        if base.VI('reset'):
+        if base.VI():
             break
 
-        if not base.EI_Click(r'data\test.png', name='test', move_func=center, value=c):
+        if not base.EI_Click_PN(r'data/test.png', name='test', move_func=base.center, value=c):
             c += 1
 
 if __name__ == "__main__":
