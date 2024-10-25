113b34b2669fdae72022f93ba7fa6ce0ba09343d
Tfregues <rafaelitoto895@gmail.com>
2024-10-25 09:09:44 -0300

    Create core.py

---
+++ core.py
@@ -0,0 +1,21 @@
+import base
+
+def run():
+    c = 0
+    while True:
+
+        if base.EI('reset'):
+            c = 0
+            break       
+
+        if base.VI('reset'):
+            break
+
+        if not base.PN(r'data\test.png', name='test', value=c):
+            c += 1
+
+if __name__ == "__main__":
+    try:  
+        run()
+    finally:
+        base.log_file.close()
